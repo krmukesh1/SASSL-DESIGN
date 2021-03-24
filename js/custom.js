1ae774da663ec9b6c1276ee531eed1eb96c6e310
email <krmukeshsingh51@gmail.com>
2021-03-24 22:40:17 +0530

    added codes

---
+++ js/custom.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $(".icon").click(function(){
+      // // $(".nav").toggle()
+      $(".icon").toggleClass('change')
+      $(".middle-header").toggleClass('active')
+      
+    });
+  });
